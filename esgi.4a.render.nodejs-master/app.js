c6d0f23413882da41da8dad12144da9a1e243edc
PES <91094335+Pierre63628@users.noreply.github.com>
2026-01-30 10:54:47 +0100

    Add DB connection

--- esgi.4a.render.nodejs-master/app.js
+++ esgi.4a.render.nodejs-master/app.js
@@ -7,8 +7,10 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
+const db = require('./db')
+
 const indexRouter = require('./routes/index')
-//const usersRouter = require('./routes/users')
+const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -23,7 +25,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-//app.use('/users', usersRouter)
+app.use('/users', usersRouter)
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
